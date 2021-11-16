1bbbf169a7186d828782307b19dbea3f287847ef
Jordan <jordansacct@gmail.com>
2021-11-16 09:56:17 -0800

    changed some copy

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -15,13 +15,13 @@ const AboutMe = () => {
                 <div className='bio'>
                     <div className=''>
                         <p>
-                        <span className='span-color'>Software Engineer</span>  based in the Bay Area. I have a found passion and dedicated enthusiasm for all things programming and learning. Simply put, I love coding. The unending stream of  <span className='span-color'>innovative technologies and information</span> . The exitement after finding the source of that pesky error you've been staring at <span className='span-color'>for just the right amount of time</span>. The creativity and balance of refactoring unpolished code to something informative, readable, and clever.
+                        <span className='span-color'>Software Engineer</span>  based in the Bay Area. I have a found passion and dedicated enthusiasm for all things programming and learning. Simply put, I love coding. The constant stream of  <span className='span-color'>innovative technologies and information</span> . The reward of finding the source of that pesky error you've been staring at <span className='span-color'>for just the right amount of time</span>. The creativity and balance of refactoring unpolished code to something informative, readable, and clever.
                         </p>
                         <p>
-                            Formerly I was an <span className='span-color'>award winning Chef with over 13 years of experience in the industry</span>. I've managed some of the most renowned restaurants in California, and was an integral part of <span className='span-color'>Team USA for the 2019 Bocuse d'Or</span> .
+                            Formerly I was an <span className='span-color'>award winning Chef with over 13 years of experience in the industry</span>. I've managed some of the most renowned restaurants in California, and I was an integral part of <span className='span-color'>Team USA for the 2019 Bocuse d'Or</span> .
                         </p>
                         <p>
-                            I pride myself on being prompt, organized, quality question asking, and working well with a team. I have pronounced experience with <span className='span-color'>Javascript, React, Redux, Node.js, Python, and Flask</span> .
+                            I pride myself on being prompt, organized, asking high quality questions, and working well with a team. I have pronounced experience with <span className='span-color'>Javascript, React, Redux, Node.js, Python, and Flask</span> .
                         </p>
                     </div>
                 </div>
